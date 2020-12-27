adebd7a7c9bfab86ec1d13307c8fad259e4f555b
mathiasjespers <mathias.jespers@student.ehb.be>
2020-12-27 14:21:47 +0100

    fixed isAuthenticated and added loading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes } from './routes';
 
 function App() {
 
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div className="loading"></div>;
+  }
+
+  
   return (
     <div className="App">
       <Router>
--- src/components/navbar/LogoutButton.js
+++ src/components/navbar/LogoutButton.js
@@ -4,12 +4,13 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 const LogoutButton = () => {
     const { logout, isAuthenticated } = useAuth0();
-    return  (
-        isAuthenticated && (
-            <button onClick={() => logout()}>
-            Log Out
+    
+    return  isAuthenticated && (
+            <button onClick={() => {
+            logout({ returnTo: window.location.origin });
+            }}>
+                Log Out
             </button>
-        )
     )
 }
 
