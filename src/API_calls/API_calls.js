dcde4833030627143171833492afe826c1395bb5
mathiasjespers <mathias.jespers@student.ehb.be>
2020-12-30 15:59:48 +0100

    api call to save score

--- src/API_calls/API_calls.js
+++ src/API_calls/API_calls.js
@@ -1,5 +1,39 @@
-const saveScore = () => {
-    return 0;
+const baseUrl = process.env.REACT_APP_API_URL;
+
+const config = {
+    headers: { Authorization: `Bearer ${token}` }
+};
+
+const saveScore = (score) => {
+    axios({
+        method: 'get',
+        url: baseUrl + '/highscore',
+        data: {
+          user: 'Finn'
+        },
+        config
+    })
+    .then((response) => {
+        console.log(response.data);
+        /*if (data = score)   {
+            axios({
+                method: 'post',
+                url: baseUrl + '/newScore',
+                data: {
+                  user: 'Finn',
+                  score: 'Williams'
+                },
+                config
+            })
+            .then((response) => {
+                console.log(response);
+            }, (error) => {
+                console.log(error);
+            });
+        }*/
+    }, (error) => {
+        console.log(error);
+    });
 }
 
 export default saveScore;
